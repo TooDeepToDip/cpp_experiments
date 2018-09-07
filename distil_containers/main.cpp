eb42d4c8d7fa4e76078c9f576219ece6a85dd320
silkinalex <silkin@po.nio1>
2018-09-07 16:47:45 +0300

    draft

---
+++ distil_containers/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <deque>
+#include <list>
+#include <vector>
+
+template<class ContainerType>                                   
+void print_container(std::ostream& out, const ContainerType & m)
+{                                                               
+    typedef typename ContainerType::const_iterator ci;          
+    for(ci it = m.begin(), end = m.end(); it != end; ++it)      
+    {                                                           
+        out << *(*it) << "\n";                                     
+    }                                                           
+}                                                               
+
+struct A
+{
+  static int m_init;
+  A() : a(++m_init) {}
+  int a;
+};
+
+int A::m_init = 2;
+
+std::ostream& operator<<(std::ostream& out, const A& a)
+{
+  return out << "A = { " << a.a << " }";
+}
+
+struct B : public A
+{
+  B(int b) : b(b) {}
+  int b;
+};
+
+std::ostream& operator<<(std::ostream& out, const B& b)
+{
+  return out << "B = { " << (const A&)b << ", " << b.b << " }";
+}
+
+template<typename T1, typename AccessF, typename SizeF, typename T2>
+T2& collect(T1& obj, AccessF get, SizeF size, T2& to)
+{
+  for(size_t i = 0; i < (obj.*size)(); ++i)
+    to.push_back((obj.*get)(i));
+  return to;
+}
+
+template<typename T1, typename T2>
+T2& distil(const T1& from, T2& to)
+{
+    std::copy(from.begin(), from.end(), std::back_inserter(to));
+    return to;
+}                                                            
+
+struct WrapperAbstract
+{
+  virtual size_t count() const = 0;
+  virtual B* get_element(size_t i) = 0;
+  //virtual const B* get_element(size_t i) const = 0; FIXME HERE error because compiler cannot resolve overloading function
+  // main.cpp:42:5: note:   template argument deduction/substitution failed:
+  // main.cpp:88:74: note:   couldn't deduce template parameter ‘AccessF’
+  // or
+  // main.cpp:42:5: note: candidate template ignored: couldn't infer template argument 'AccessF'
+  std::deque<B*> lb;
+};
+
+struct Wrapper : public WrapperAbstract
+{
+  Wrapper()
+  {
+    for(int i = 0; i < 3; ++i)
+      lb.push_back(new B(i));
+  }
+  size_t count() const { return lb.size(); }
+  B* get_element(size_t i) { return lb[i]; }
+  //const B* get_element(size_t i) const { return lb[i]; }
+};
+
+int main()
+{
+  //std::list<B*> lb;
+  //for(int i = 0; i < 3; ++i)
+  //  lb.push_back(new B(i));
+  //Wrapper w;
+  WrapperAbstract* wp = new Wrapper();
+  print_container(std::cout, wp->lb);
+  //std::list<A*> la;
+  //std::copy(lb.begin(), lb.end(), std::back_inserter(la));
+  std::list<B*> lb;
+  //collect(w, &Wrapper::get_element, &Wrapper::count, lb);
+  collect(*wp, &WrapperAbstract::get_element, &WrapperAbstract::count, lb);
+  std::list<A*> la;
+  distil(lb, la);
+  print_container(std::cout, la);
+  std::list<A*> va;
+  distil(lb, va);
+  print_container(std::cout, va);
+}
